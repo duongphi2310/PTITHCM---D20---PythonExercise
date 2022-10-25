91b241f6a16681bf8821e7534642c3509c6d7e2c
duongphi2310 <115022796+duongphi2310@users.noreply.github.com>
2022-10-25 22:18:22 +0700

    Update EX1.1.py
    Write a program to convert minutes into a number of years and days. 
    Test Data
    Input the number of minutes: 3456789
    Expected Output : 6 years, 210 days

--- EX1.1.py
+++ EX1.1.py
@@ -1,3 +1,9 @@
+# Write a program to convert minutes into a number of years and days.
+
+# Test Data
+# Input the number of minutes: 3456789
+# Expected Output : 6 years, 210 days
+
 phutnam = 60 * 24 * 365
 nhap = int(input())
 year = nhap // phutnam
