72eb2a8d2ed43e4a1d36519cf67d69606835b377
duongphi2310 <115022796+duongphi2310@users.noreply.github.com>
2022-12-17 21:27:32 +0700

    EX 2.7. Write A Function To Convert Vietnamese Characters Into ASCII Ones.
    EX 2.7. Write A Function To Convert Vietnamese Characters Into ASCII Ones.

---
+++ EX 2.7.py
@@ -0,0 +1,26 @@
+# Write a function convert_to_ascii
+# to convert Vietnamese characters into ASCII ones.
+
+# Test data:
+# Input:  Xin chào Việt Nam
+# Output: Xin chao Viet Nam
+
+import re
+def vn(s):
+    s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(r'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(r'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(r'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(r'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(r'[ìíịỉĩ]', 'i', s)
+    s = re.sub(r'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(r'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(r'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(r'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(r'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(r'[Đ]', 'D', s)
+    s = re.sub(r'[đ]', 'd', s)
+    return s
+s = input()
+print(vn(s))
